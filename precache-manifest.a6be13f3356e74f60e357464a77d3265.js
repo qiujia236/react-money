31ccdf0ef68b3ab7df8f93d76c9200145ccae18e
wb <wbak123@sina.com>
2020-08-27 19:51:50 +0800

    first commit

---
+++ precache-manifest.a6be13f3356e74f60e357464a77d3265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66ed724929efe7cdb7e7cc7b979f206",
+    "url": "/react-money/index.html"
+  },
+  {
+    "revision": "84f435d5e01ead532497",
+    "url": "/react-money/static/css/main.232f9e43.chunk.css"
+  },
+  {
+    "revision": "c5d3bad7ca5661f9ba75",
+    "url": "/react-money/static/js/2.964b0b1e.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/react-money/static/js/2.964b0b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f435d5e01ead532497",
+    "url": "/react-money/static/js/main.696eb862.chunk.js"
+  },
+  {
+    "revision": "2f16d0ab4579191c2a40",
+    "url": "/react-money/static/js/runtime-main.a2c0ea71.js"
+  }
+]);
